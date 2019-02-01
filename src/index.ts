0d022b0ca8653be1e487d0fa03e27c6c75e7deb1
Jason-Morales <jason.morales@imaginelearning.com>
2019-02-01 11:47:32 -0500

    Full response added via new param on queries

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ class rxios {
     this._httpClient = axios.create(options);
   }
 
-  private _makeRequest<T>(method: string, url: string, queryParams?: object, body?: object) {
+  private _makeRequest<T>(method: string, url: string, queryParams?: object, body?: object, fullResponse: boolean = false) {
     let request: AxiosPromise<T>;
     switch (method) {
       case 'GET':
@@ -36,7 +36,7 @@ class rxios {
     }
     return new Observable<T>(subscriber => {
       request.then(response => {
-        subscriber.next(response.data);
+        subscriber.next(fullResponse ? response : response.data);
         subscriber.complete();
       }).catch((err: Error) => {
         subscriber.error(err);
@@ -45,24 +45,24 @@ class rxios {
     });
   }
 
-  public get<T>(url: string, queryParams?: object) {
-    return this._makeRequest<T>('GET', url, queryParams);
+  public get<T>(url: string, queryParams?: object, fullResponse?: boolean) {
+    return this._makeRequest<T>('GET', url, queryParams, undefined, fullResponse);
   }
 
-  public post<T>(url: string, body: object, queryParams?: object) {
-    return this._makeRequest<T>('POST', url, queryParams, body);
+  public post<T>(url: string, body: object, queryParams?: object, fullResponse?: boolean) {
+    return this._makeRequest<T>('POST', url, queryParams, body, fullResponse);
   }
   
-  public put<T>(url: string, body: object, queryParams?: object) {
-    return this._makeRequest<T>('PUT', url, queryParams, body);
+  public put<T>(url: string, body: object, queryParams?: object, fullResponse?: boolean) {
+    return this._makeRequest<T>('PUT', url, queryParams, body, fullResponse);
   }
 
-  public patch<T>(url: string, body: object, queryParams?: object) {
-    return this._makeRequest<T>('PATCH', url, queryParams, body);
+  public patch<T>(url: string, body: object, queryParams?: object, fullResponse?: boolean) {
+    return this._makeRequest<T>('PATCH', url, queryParams, body, fullResponse);
   }
   
-  public delete(url: string, queryParams?: object) {
-    return this._makeRequest('DELETE', url, queryParams);
+  public delete(url: string, queryParams?: object, fullResponse?: boolean) {
+    return this._makeRequest('DELETE', url, queryParams, undefined, fullResponse);
   }
 }
 
